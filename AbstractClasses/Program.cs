aa8f723e8e8b459ccb6c1739f19ad05f680585a5
Adotey2 <alloteyleslie8@gmail.com>
2025-08-09 07:20:28 +0000

    done my first 2 programs

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+abstract class Shape
+{
+    // Abstract method (no body here)
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a Circle with radius 5
+        Shape circle = new Circle(5);
+        Console.WriteLine("Circle Area: " + circle.GetArea());
+
+        // Create a Rectangle with width 4 and height 6
+        Shape rectangle = new Rectangle(4, 6);
+        Console.WriteLine("Rectangle Area: " + rectangle.GetArea());
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create instances
+        Animal genericAnimal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        // Call MakeSound() on each instance
+        genericAnimal.MakeSound(); // Output: Some generic sound
+        dog.MakeSound();           // Output: Bark
+        cat.MakeSound();           // Output: Meow
+    }
+}
